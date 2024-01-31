8fb2bb9ae743d01d14767d382fdbc23e209dad7f
Isaac Moraes de Melo <isaacmoraesdemelo@gmail.com>
2024-01-31 09:45:50 -0400

    desafio

---
+++ src/DESAFIO.java
@@ -0,0 +1,50 @@
+import entities.TaxPayer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import entities.TaxPayer;
+import java.util.Scanner;
+
+public class DESAFIO {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        double rendaAnualSalarial; double rendaAnualDeServicos;
+        double rendaAnualDaCapital; double gastosComMedicos;
+        double gastosEscolares;
+
+        System.out.print("Quantos contribuintes você vai digitar? ");
+        int quantity = sc.nextInt();
+        List<TaxPayer> list = new ArrayList<>();
+
+        for (int i = 0; i < quantity; i++) {
+            System.out.println("Digite os dados do" + (i+1) +"o contribuinte:");
+
+            System.out.print("Renda anual com salário: ");
+            rendaAnualSalarial = sc.nextDouble();
+
+            System.out.print("Renda anual com prestação de serviço: ");
+            rendaAnualDeServicos = sc.nextDouble();
+
+            System.out.print("Renda anual com ganho de capital: ");
+            rendaAnualDaCapital = sc.nextDouble();
+
+            System.out.print("Gastos médicos: ");
+            gastosComMedicos = sc.nextDouble();
+
+            System.out.print("Gastos educacionais: ");
+            gastosEscolares = sc.nextDouble();
+
+
+            list.add(new TaxPayer(rendaAnualSalarial,rendaAnualDeServicos,rendaAnualDaCapital,gastosComMedicos,gastosEscolares));
+        }
+
+
+
+        for (int i = 0; i < quantity; i++){
+            System.out.println("Resumo do" + (i+1) + "o contribuinte:");
+            System.out.println("Imposto bruto total: ");
+        }
+    }
+}
